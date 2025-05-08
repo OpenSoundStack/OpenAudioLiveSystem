9fd3578282a19a652f62f050148b1442f6758a5c
Mathis_D <mathis.d@acistec.fr>
2025-05-08 22:45:24 +0200

    added framing to ui

--- coreui/ui/PipeVisualizer.cpp
+++ coreui/ui/PipeVisualizer.cpp
@@ -21,7 +21,9 @@ void PipeVisualizer::set_pipe_content(PipeDesc *desc) {
     PipeDesc* current_desc = desc;
     while (current_desc != nullptr) {
         current_desc->desc_content->setParent(this);
-        ui->elem_container->insertWidget(0, current_desc->desc_content);
+
+        auto* container_layout = (QVBoxLayout*)ui->elem_container->layout();
+        container_layout->insertWidget(0, current_desc->desc_content);
 
         if (current_desc->next_pipe_elem.has_value()) {
             current_desc = current_desc->next_pipe_elem.value();
@@ -36,7 +38,7 @@ void PipeVisualizer::set_pipe_content(PipeDesc *desc) {
 void PipeVisualizer::clear_current() {
     PipeDesc* current_desc = m_desc;
     while (current_desc != nullptr) {
-        ui->elem_container->removeWidget(current_desc->desc_content);
+        ui->elem_container->layout()->removeWidget(current_desc->desc_content);
 
         current_desc = current_desc->next_pipe_elem.value();
     }
