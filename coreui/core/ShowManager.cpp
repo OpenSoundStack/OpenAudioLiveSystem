a50aab1349f75026848338fa574de39929dd9791
Mathis_D <mathis.d@acistec.fr>
2025-05-17 22:40:20 +0200

    added networking to ui

--- coreui/core/ShowManager.cpp
+++ coreui/core/ShowManager.cpp
@@ -8,6 +8,30 @@ ShowManager::~ShowManager() {
 
 }
 
+bool ShowManager::init_console() {
+    constexpr char device_name[32] = "CORE I Control Surface";
+
+    PeerConf infos{};
+    infos.dev_type = DeviceType::CONTROL_SURFACE;
+    infos.iface = "virbr0";
+    infos.sample_rate = SamplingRate::SAMPLING_96K;
+    infos.uid = 200;
+    infos.topo.phy_in_count = 0;
+    infos.topo.phy_out_count = 0;
+    infos.topo.pipes_count = 0;
+    memcpy(infos.dev_name, device_name, 32);
+
+    m_nmapper = std::make_shared<NetworkMapper>(infos);
+    if (!m_nmapper->init_mapper(infos.iface)) {
+        return false;
+    }
+
+    m_nmapper->launch_mapping_process();
+
+    return true;
+}
+
+
 void ShowManager::add_pipe() {
     m_ui_show_content.append(new PipeVisualizer{
         (int)m_ui_show_content.size()
--- coreui/core/ShowManager.h
+++ coreui/core/ShowManager.h
@@ -4,15 +4,22 @@
 #include "coreui/ui/PipeVisualizer.h"
 #include "coreui/ui/SignalWindow.h"
 
+#include "OpenAudioNetwork/common/NetworkMapper.h"
+#include "OpenAudioNetwork/peer/peer_conf.h"
+
 class ShowManager {
 public:
     ShowManager();
     ~ShowManager();
 
+    bool init_console();
+
     void add_pipe();
     void update_page(SignalWindow* swin);
 private:
     QList<PipeVisualizer*> m_ui_show_content;
+
+    std::shared_ptr<NetworkMapper> m_nmapper;
 };
 
 
